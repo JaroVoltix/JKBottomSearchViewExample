f3bc126564087c967d12dce4201bdfbd50d17060
Jarosław Krajewski <jaroslaw.krajewski94@gmail.com>
2018-04-11 22:28:14 +0200

    Example customization of JKBottomSearchView

---
+++ JKBottomSearchViewExample/JKBottomSearchView.framework/README.md
@@ -0,0 +1,79 @@
+# JKBottomSearchView
+[![Build Status](https://travis-ci.org/JaroVoltix/JKBottomSearchView.svg?branch=master)](https://travis-ci.org/JaroVoltix/JKBottomSearchView)
+
+## Installation
+
+### Cocapods
+``` Coming soon```
+
+### Carthage 
+``` github "JeyKeyCom/JKBottomSearchView"```
+
+## Requirment
+ - iOS 9+
+ - Xcode 9+
+ - Swift 4.1+
+ 
+## Usage
+Create JKBottomSearchView 
+```
+func viewDidLoad(){
+    super.viewDidLoad()
+    let searchView = JKBottomSearchView()
+    view.addSubview(searchView)
+}
+```
+### DataSource & Delegate
+JKBottomSearchViewDataSource is and alias to UITableViewDataSource. 
+
+```
+extension ViewController:JKBottomSearchViewDataSource{}
+```
+```
+searchView.dataSource = self
+```
+
+JKBottomSearchViewDelegate is an alias for UITableViewDelegate & UISearchBarDelegate.
+
+```
+extension ViewController:JKBottomSearchViewDelegate{}
+```
+```
+searchView.delegate = self
+```
+
+### Customization
+```
+//Change blur effect. Default nil
+searchView.blurEffect = UIBlurEffect(style: .dark)
+
+//Any non tableView and searchBar customization should be performed on contentView
+searchView.contentView.backgroundColor = .red
+
+//Customizing searchBar
+searchView.barStyle = .black 
+searchView.searchBarStyle = .minimal 
+searchView.searchBarTintColor = .black
+searchView.placeholder = "What are you looking for"
+searchView.showsCancelButton = true
+searchView.enablesReturnKeyAutomatically = true
+
+//Customizing searchBar textField
+let textField = searchView.searchBarTextField
+textField.textColor = .red
+
+//Customizing tableView
+//You can access tableView and customize as You like by tableView property
+searchView.tableView.isScrollEnabled = false
+
+//Customizing expansion 
+searchView.minimalYPosition = 100 // distance from top after fully expanding 
+searchView.fastExpandindTime = 0.1
+searchView.slowExpandingTime = 2
+searchView.toggleExpand(.fullyExpanded,fast:false) // fast parameter is optional, default false
+```
+
+## License
+Icons used in example available at: https://icons8.com
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
--- JKBottomSearchViewExample/ViewController.swift
+++ JKBottomSearchViewExample/ViewController.swift
@@ -3,15 +3,35 @@
 //  JKBottomSearchViewExample
 //
 //  Created by Jarosław Krajewski on 11/04/2018.
-//  Copyright © 2018 com.jerronimo. All rights reserved.
+//  Copyright © 2018 Jarosław Krajewski. All rights reserved.
 //
 
 import UIKit
+import JKBottomSearchView
+private let identifier = "cellIdentifier"
 
 class ViewController: UIViewController {
+    let images:[UIImage] = [#imageLiteral(resourceName: "train"),#imageLiteral(resourceName: "pin"),#imageLiteral(resourceName: "arrow")]
+    var searchView = JKBottomSearchView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let backgroundImageView = UIImageView(image: #imageLiteral(resourceName: "Screen Shot 2018-04-10 at 23.01.00"))
+        backgroundImageView.frame = view.frame
+        view.addSubview(backgroundImageView)
+
+        searchView.dataSource = self
+        searchView.delegate = self
+
+        searchView.placeholder = "What are you looking for?"
+        searchView.searchBarStyle = .minimal
+        searchView.tableView.backgroundColor = .clear
+        searchView.contentView.layer.cornerRadius = 10
+        searchView.contentView.backgroundColor = UIColor.white.withAlphaComponent(0.9)
+
+        let nib = UINib(nibName: "TableViewCell", bundle: .main)
+        searchView.tableView.register(nib, forCellReuseIdentifier: identifier)
+        view.addSubview(searchView)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +39,37 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension ViewController: JKBottomSearchDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as! TableViewCell
+
+        cell.placeLabel.text = "Place \(indexPath.row)"
+        cell.addressLabel.text = "Adress of this place"
+        cell.backgroundColor = .clear
+
+        let image = images[indexPath.row % images.count]
+        cell.imageView?.image = image.withRenderingMode(.alwaysTemplate)
+        cell.imageView?.tintColor = .white
+        cell.circleView.backgroundColor = .red
+        cell.circleView.layer.cornerRadius = cell.circleView.frame.width/2
+        cell.selectionStyle = .none
+
+        return cell
+    }
+}
+
+extension ViewController: JKBottomSearchViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+}
