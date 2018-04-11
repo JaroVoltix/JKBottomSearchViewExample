93486fcacf29d218e9d717e522eb8bf48a94ae86
Jarosław Krajewski <jaroslaw.krajewski94@gmail.com>
2018-04-11 21:37:06 +0200

    Initial Commit

---
+++ JKBottomSearchViewExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JKBottomSearchViewExample
+//
+//  Created by Jarosław Krajewski on 11/04/2018.
+//  Copyright © 2018 com.jerronimo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
